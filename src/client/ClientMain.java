c1df986d88add00647b4a2250da6bcfefaf8f6c5
Svein-Kåre Bjørnsen <sveinkare@gmail.com>
2020-11-03 11:23:56 +0100

    main classes set up, as well as artifacts

--- src/client/ClientMain.java
+++ src/client/ClientMain.java
@@ -1,7 +1,7 @@
 package client;
 
-public class Main {
-    static void main(String[] args) {
+public class ClientMain {
+    public static void main(String[] args) {
         System.out.println("Hello, I'm the client!");
     }
 }
--- src/server/ServerMain.java
+++ src/server/ServerMain.java
@@ -1,7 +1,7 @@
 package server;
 
-public class Main {
-    static void main(String[] args) {
+public class ServerMain {
+    public static void main(String[] args) {
         System.out.println("Hello, I'm the server!");
     }
 }
