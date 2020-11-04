c6aeb09dc9622c4b98f239a68c3d465acd3ee823
charleyv <charleyv@stud.ntnu.no>
2020-11-04 11:41:14 +0100

    Added method of returning hand to player via token in interface

--- src/client/GameStateTracker.java
+++ src/client/GameStateTracker.java
@@ -1,5 +1,8 @@
 package client;
 
+import java.util.ArrayList;
+
 public interface GameStateTracker {
     String getActivePlayerID(); // returns nick or unique identifier of current player
+    ArrayList<Card> getPlayerHand(String token);   // Returns an arraylist to the player with the respective token
 }
