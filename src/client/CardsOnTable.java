2d4704a8ddbe091a158aba0d1466d8f89d4f8ee2
Ruslebiff <joakivb@stud.ntnu.no>
2020-11-16 19:24:13 +0100

    fixed image loading

--- src/client/CardsOnTable.java
+++ src/client/CardsOnTable.java
@@ -13,16 +13,14 @@ import java.util.ArrayList;
 // Class for GUI of the cards played on the table
 public class CardsOnTable extends JPanel{
     private BufferedImage image;    // Image of green felt
-    private final String filePath;  // Path to image of green felt
     private ArrayList<Card> lastFourCards = new ArrayList<>();   // The last three cards played
     private GameStateTracker stateTracker;
 
     public CardsOnTable(int widht, int height, GameStateTracker sT) {
         stateTracker = sT;
-        // Renders the green filt
-        this.filePath = "./resources/green_fabric.jpg"; // Filepath
+        // Renders the green felt
         try {
-            image = ImageIO.read(new File(filePath));       // Read the image
+            image = ImageIO.read(ClientMain.class.getResourceAsStream("/green_fabric.jpg"));       // Read the image
         } catch (IOException ex) {
             ex.printStackTrace();
         }
