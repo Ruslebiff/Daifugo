7f4a39d352fa946a7e6ce31c0e2da1c147fb50ee
Ruslebiff <joakivb@stud.ntnu.no>
2020-11-18 17:22:36 +0100

    read rules from file

--- src/common/Info.java
+++ src/common/Info.java
@@ -1,12 +1,19 @@
 package common;
 
+import client.ClientMain;
+
+import javax.imageio.ImageIO;
 import javax.swing.*;
+import java.io.*;
+import java.nio.charset.Charset;
+import java.nio.charset.StandardCharsets;
 import java.util.Arrays;
 
 public class Info {
     private static double version;
     private static String[] contributors;
     private final int year;
+    private InputStream textInput;
 
 
     public Info() {
@@ -40,18 +47,23 @@ public class Info {
     }
 
     public void showRulesWindow() {
-        JFrame f = new JFrame("Rules");//creating instance of JFrame
-        int SCREEN_HEIGHT = 150;
-        int SCREEN_WIDTH = 200;
+        JFrame f=new JFrame("Rules");//creating instance of JFrame
+        int SCREEN_HEIGHT = 600;
+        int SCREEN_WIDTH = 600;
 
-        JTextArea textArea = new JTextArea(40, 20);
+        JTextArea textArea = new JTextArea();
         JScrollPane scrollPane = new JScrollPane(textArea);
         textArea.setEditable(false);
 
-        textArea.setText("Rules be here"); // TODO: read rules
+        // Read the file
+        try (BufferedReader textInput = new BufferedReader(new InputStreamReader(ClientMain.class.getResourceAsStream("/rules.txt"), Charset.defaultCharset()))) {
+            textArea.read(textInput, "File");
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
 
         f.setSize(SCREEN_WIDTH,SCREEN_HEIGHT);  // Frame size
-        f.add(textArea);
+        f.add(scrollPane);
         f.setLocationRelativeTo(null);          // Place in middle of screen
         f.setVisible(true);                     // Frame visible
     }
