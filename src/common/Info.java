ac297677de770b13454486872a36995c4cb0fc8a
Ruslebiff <joakivb@stud.ntnu.no>
2020-11-18 19:00:29 +0100

    About done

--- src/common/Info.java
+++ src/common/Info.java
@@ -2,52 +2,78 @@ package common;
 
 import client.ClientMain;
 
-import javax.imageio.ImageIO;
 import javax.swing.*;
+import java.awt.*;
 import java.io.*;
 import java.nio.charset.Charset;
-import java.nio.charset.StandardCharsets;
-import java.util.Arrays;
+import java.util.ArrayList;
+import java.util.stream.Collectors;
 
 public class Info {
-    private static double version;
-    private static String[] contributors;
-    private final int year;
-    private InputStream textInput;
-
+    private double version;
+    private static ArrayList<String> contributors;
+    private int year;
 
     public Info() {
-        this.contributors = new String[]{"person 1", "person 2", "person 3"}; // TODO: get contributors
+        this.contributors = getContributors(); // TODO: get contributors
         this.version = 1.0;
         this.year = 2020;
     }
 
-    public static double getVersion() {
-        return version;
-    }
-    public static String getContributors() {
-        return Arrays.toString(contributors);
+    public static ArrayList<String> getContributors() {
+        ArrayList<String> c = new ArrayList<>();
+        // Read the file
+        try (BufferedReader textInput = new BufferedReader(new InputStreamReader(ClientMain.class.getResourceAsStream("/contributors.txt"), Charset.defaultCharset()))) {
+            String line = textInput.readLine();
+            while (line != null) {
+                c.add(line);
+                line = textInput.readLine();
+            }
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return c;
     }
 
     public void showAboutWindow() {
-        JFrame f = new JFrame("About");//creating instance of JFrame
-        int SCREEN_HEIGHT = 150;
-        int SCREEN_WIDTH = 200;
+        JFrame f = new JFrame("About Daifugo");
+        int SCREEN_HEIGHT = 200;
+        int SCREEN_WIDTH = 300;
+        f.setLayout(new FlowLayout());
 
-        JTextArea textArea = new JTextArea(40, 20);
-        JScrollPane scrollPane = new JScrollPane(textArea);
-        textArea.setEditable(false);
+        JPanel panel = new JPanel();
+        panel.setBounds(0, 0, SCREEN_WIDTH, SCREEN_HEIGHT);
+        panel.setLayout(new BoxLayout(panel, BoxLayout.Y_AXIS));
+        f.add(panel);
 
-        textArea.setText("Version: " + getVersion() + "\n" + "Contributors: " + getContributors()); // TODO: make contributors text better
+        JLabel labelVersion = new JLabel( "Daifugo - version " + this.version);
+        labelVersion.setAlignmentX(Component.CENTER_ALIGNMENT);
+        JLabel labelYear = new JLabel("Copyright © " + this.year + " Daifugo creators");
+        labelYear.setAlignmentX(Component.CENTER_ALIGNMENT);
+        JLabel emptyLine = new JLabel(" ");
+        JLabel labelContributors = new JLabel("Contributors:");
+        labelContributors.setAlignmentX(Component.CENTER_ALIGNMENT);
+        JLabel labelGithub = new JLabel("github.com/Ruslebiff/Daifugo");
+        labelGithub.setAlignmentX(Component.CENTER_ALIGNMENT);
+        panel.add(labelVersion);
+        panel.add(labelYear);
+        panel.add(labelGithub);
+        panel.add(emptyLine);
+        panel.add(labelContributors);
+        for (String c : contributors) {
+            JLabel cLabel = new JLabel(c);
+            cLabel.setAlignmentX(Component.CENTER_ALIGNMENT);
+            panel.add(cLabel);
+        }
 
-        f.setSize(SCREEN_WIDTH,SCREEN_HEIGHT);  // Frame size
-        f.add(textArea);
+        f.setSize(SCREEN_WIDTH,SCREEN_HEIGHT);
         f.setLocationRelativeTo(null);          // Place in middle of screen
         f.setVisible(true);                     // Frame visible
     }
 
     public void showRulesWindow() {
-        JFrame f=new JFrame("Rules");//creating instance of JFrame
+        JFrame f=new JFrame("Rules");
         int SCREEN_HEIGHT = 600;
         int SCREEN_WIDTH = 600;
 
@@ -62,7 +88,9 @@ public class Info {
             e.printStackTrace();
         }
 
-        f.setSize(SCREEN_WIDTH,SCREEN_HEIGHT);  // Frame size
+        textArea.setBackground(Color.lightGray);
+
+        f.setSize(SCREEN_WIDTH,SCREEN_HEIGHT);
         f.add(scrollPane);
         f.setLocationRelativeTo(null);          // Place in middle of screen
         f.setVisible(true);                     // Frame visible
